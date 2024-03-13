211719e74620664c2af2ab46340db1a926ffbfbd
Madhu <madhuvanthishankar@gmail.com>
2024-03-13 19:42:37 +0530

    delete occurrences in DLL

---
+++ deleteOccInDll.cpp
@@ -0,0 +1,22 @@
+Node * deleteAllOccurrences(Node* head, int k) {
+    Node* temp = head;
+    while(temp!=NULL){
+        if(temp->data == k){
+            if(temp == head){
+                head = head->next;
+            }
+            Node* nextNode = temp->next;
+            Node* prevNode = temp->prev;
+
+            if(nextNode) nextNode->prev = prevNode;
+            if(prevNode) prevNode->next = nextNode;
+
+            free(temp);
+            temp = nextNode; 
+        }
+        else{
+            temp = temp->next;
+        }
+    }
+    return head;
+}
